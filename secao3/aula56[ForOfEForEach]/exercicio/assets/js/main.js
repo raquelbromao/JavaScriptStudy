68333391f89ead9c427bfa08cd6d0c030d2715f1
Raquel Romão <raquelbromao@brq.com>
2021-04-15 14:11:26 -0300

    feito exercicio referente a aula 57

--- secao3/aula56[ForOfEForEach]/exercicio/assets/js/main.js
+++ secao3/aula56[ForOfEForEach]/exercicio/assets/js/main.js
@@ -1,3 +1,21 @@
-alert(1);
+/**
+ * .querySelector() -> retorna um único elemento (Element), o primeiro que bater com o arg enviado 
+ * .querySelectorAll() -> retorna o conjunto de elementos (NodeList)
+ */ 
 
-const paragrafos = window.document.querySelector('.paragrafos'); //selecionando via classe
+// querySelector so retorna um elemento
+const divParagrafos = window.document.querySelector('.paragrafos'); //selecionando via classe
+// selecionar uma coleção de elementos
+const paragrafos = divParagrafos.querySelectorAll('p');
+
+// pegar a cor do background do body
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody);
+
+// Mudar a cor do background dos paragrafos para o backgroundColorBody e alterar a cor do texto
+for (const p of paragrafos) {
+  console.log(p);
+  p.style.backgroundColor = backgroundColorBody;
+  p.style.color = 'blue';
+}
